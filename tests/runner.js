141927cc60d37aff570a5c508c3fd42e99be8aba
Jared Hanson <jaredhanson@gmail.com>
2012-08-19 17:18:07 -0700

    Initial commit.

---
+++ tests/runner.js
@@ -0,0 +1,19 @@
+require.config({
+  paths:{
+    'class': '../',
+    'mocha': 'vendor/mocha/1.0.1/mocha',
+    'chai': 'vendor/chai/0.5.2/chai'
+  }
+});
+
+require(['require',
+         'mocha',
+         'chai'],
+function(require, _mocha, _chai) {
+  mocha.setup('bdd');
+  
+  require(['test.class'],
+  function() {
+    mocha.run();
+  });
+});
